4f7948ec2bc53078c084ab65c97a2a5915496b56
Syed Ameer Hamza Hussaini <syedameerhamzahussaini@gmail.com>
2026-02-03 23:13:52 +0530

    Day1:Initial setup and Hello World

---
+++ scr/python_fundamental.py
@@ -0,0 +1,50 @@
+name="ameer hamza"
+print(name)
+
+print(type(name))
+
+age=12.0
+print(type(age))
+
+a=True
+print(type(a))
+
+num1=20
+num2=30
+result=num1+num2
+print(result)
+
+print(num1*num2)
+
+user_name=input("enter your name")
+print("your name",user_name)
+
+user_age= int (input("enter your age"))
+
+#task1
+
+user_name1=input("enter your name")
+user_age1=input("enter your age")
+user_age1=int(user_age1)
+user_age1+=4
+print(f"hey {user_name1},you will be {user_age1}years old in 2030")
+
+#task2
+
+total_bill=float(input("enter the total bill"))
+no_of_people=int(input("enter total number of people"))
+share_per_person=total_bill/no_of_people
+print(f"total bill:{total_bill} each person pays {share_per_person}")
+
+print(type(total_bill))
+
+#task 3
+
+item_name="laptop"
+quantity=2
+price=499.99
+in_stock=True
+print(item_name,quantity,price,in_stock)
+
+total_cost=(quantity*price)
+print(total_cost)
