6bc0581f9b012c685b8a0dd2edc5d10068db54c8
Syed Ameer Hamza Hussaini <syedameerhamzahussaini@gmail.com>
2026-02-06 16:30:38 +0530

    Day5:day5_main

---
+++ scr/day5_math_operations.py
@@ -0,0 +1,28 @@
+
+def add(a,b):
+    add = a+b
+    return add
+
+def sub(a,b):
+    sub = a-b
+    return sub
+
+def multiplication(a,b):
+    multiplication = a*b
+    return multiplication
+    
+
+
+#task2
+def power(base, exp):
+    return base ** exp
+
+# Function to calculate average of numbers in a list
+def average(numbers_list):
+    if len(numbers_list) == 0:
+        return 0
+    return sum(numbers_list) / len(numbers_list)
+
+
+
+
